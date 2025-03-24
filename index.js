ee691ee4983068bd65b0277147e080616e902836
RalucaZota <zotaraluca649@gmail.com>
2025-03-24 21:29:54 +0200

    wip for deleting functionality

--- index.js
+++ index.js
@@ -1,23 +1,61 @@
 const input = document.querySelector('input');
-const addTaskButton = document.querySelector('ad-task');
+const addTaskButton = document.querySelector('add-task');
 const tasksParent = document.createElement('ul');
 tasksParent.classList.add('tasks-parent');
+const tasksList = [];
+let idCounter = 0;
+let newTask;
+let removeTaskButton;
 
 const updateValue = (event) => {
-    input.value = event.target.value
-}
-input.addEventListener( 'change', (event) => updateValue(event));
+  input.value = event.target.value;
+};
+input.addEventListener('change', (event) => updateValue(event));
+input.addEventListener('keypress', (event) => handleEnterKey(event));
 
 const addTask = () => {
-    const newTask = document.createElement("li");
-    tasksParent.appendChild(newTask);
-    console.log(tasksParent);
-    newTask.innerHTML = input.value;   
-    cleanInput();
-}
+  if (input.value.trim() === '') return;
+  newTask = document.createElement('li');
+  newTask.setAttribute('id', idCounter++);
+  console.log(newTask);
+
+  tasksParent.appendChild(newTask);
+  newTask.innerHTML = input.value;
+  // tasksList.push({value: input.value, id: idCounter++})
+  console.log(tasksList);
+  
+  addTasktoList();
+  addRemoveTaskButton();
+  cleanInput();
+};
 
 const cleanInput = () => {
-    input.value = ""; 
-}
+  input.value = '';
+};
+
+const handleEnterKey = (event) => {
+  if (event.key === 'Enter') {
+    addTask();
+  }
+};
+
+const addRemoveTaskButton = () => {
+  const removeTaskButton = document.createElement('button');
+  removeTaskButton.innerHTML = 'Remove task';
+  removeTaskButton.addEventListener('click', () => removeTask());
+  console.log('test');
+  
+  newTask.appendChild(removeTaskButton);
+};
+
+const removeTask = () => {
+  console.log('tasksList', tasksList)
+};
+
+const addTasktoList = () => {
+  tasksList.push({ value: input.value, id: newTask.id });
+  console.log(tasksList);
+  return tasksList;
+};
 
 document.body.appendChild(tasksParent);
